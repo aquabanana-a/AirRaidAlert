82660b6d9b91db1cbf007cf79cc5c314f3025439
Serg Dobranos <serg.evoke@gmail.com>
2023-04-13 14:51:14 +0300

    Added in-app-review

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation(Dependencies.AndroidX.navigation)
     implementation(Dependencies.AndroidX.navigationUi)
 
+    implementation("com.google.android.play:core:1.8.0")
+    implementation("com.google.android.play:core-ktx:1.8.1")
+
     // Unit tests
     testImplementation(Dependencies.testLibraries)
 }
--- app/src/main/java/com/banana/ara/view/about/AboutFragment.kt
+++ app/src/main/java/com/banana/ara/view/about/AboutFragment.kt
@@ -1,11 +1,16 @@
 package com.banana.ara.view.about
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.banana.ara.R
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.review.testing.FakeReviewManager
 
 class AboutFragment : Fragment() {
 
@@ -19,5 +24,28 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.rateButton).setOnClickListener {
+            requestReviewFlow(requireActivity())
+        }
+    }
+
+    private fun requestReviewFlow(activity: Activity) {
+        val reviewManager = ReviewManagerFactory.create(activity) //FakeReviewManager(activity)
+        val requestReviewFlow = reviewManager.requestReviewFlow()
+
+        requestReviewFlow.addOnCompleteListener { request ->
+            if (request.isSuccessful) {
+                val reviewInfo = request.result
+                val flow = reviewManager.launchReviewFlow(activity, reviewInfo)
+
+                flow.addOnCompleteListener {
+                    Toast.makeText(activity, "Thank for your feedback!", Toast.LENGTH_LONG).show()
+                }
+
+            } else {
+                Toast.makeText(activity, "Something went wrong :(", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
